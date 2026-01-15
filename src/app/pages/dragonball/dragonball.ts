ae7509fa5a3d51feb06ad8918d7f7103f4c54f61
Marvin S. Redin Alominas <msredin@redcolsa.local>
2026-01-15 12:21:32 -0500

    for y if

--- src/app/pages/dragonball/dragonball.ts
+++ src/app/pages/dragonball/dragonball.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, computed, signal } from '@angular/core';
+interface Character { // Definición de la interfaz Character
+  id: number;
+  name: string;
+  powerLevel: number;
+}
 
 @Component({
   selector: 'app-dragonball',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './dragonball.html',
 })
 export class Dragonball {
+  characters = signal<Character[]>([ // Señal para almacenar los personajes
+    {id: 1, name: 'Goku', powerLevel: 9001},
+    {id: 2, name: 'Vegeta', powerLevel: 8500},
+    {id: 3, name: 'Piccolo', powerLevel: 3000},
+    {id: 4, name: 'Yamcha', powerLevel: 500},
+  ]);
 
+  // powerClasses = computed (() =>{
+  //   return {
+  //     'text-danger': true,
+  //   };
+  // });
 }

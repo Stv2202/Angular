d41ead610521ba544d077b1ee74da2d7fba39210
Marvin S. Redin Alominas <msredin@redcolsa.local>
2026-01-15 11:07:27 -0500

    navbar - routerLink - routerLinkActive

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,24 @@
 import { Routes } from '@angular/router';
 import { Counter } from './pages/counter/counter';
 import { Hero } from './pages/hero/hero';
+import { Dragonball } from './pages/dragonball/dragonball';
 
 export const routes: Routes = [
     {
-        path: 'counter',
+        path: '',
         component: Counter,
     },
     {
         path: 'hero',
         component: Hero,
     },
+    {
+        path: 'dragonball',
+        component: Dragonball,
+    },
+    {
+        path: '**',
+        redirectTo: '', // Redireccionar a la ruta raíz o a una página de inicio predeterminada, en este caso a la pagina de Counter
+    }
+    
 ];
